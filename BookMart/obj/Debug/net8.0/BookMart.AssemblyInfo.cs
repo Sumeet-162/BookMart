9aafdceed6f8afc057089737b01f9be7eab3b230
sumeet <sumeet2703@gmail.com>
2025-05-23 15:27:53 +0530

    base folder structure added

--- BookMart/obj/Debug/net8.0/BookMart.AssemblyInfo.cs
+++ BookMart/obj/Debug/net8.0/BookMart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11ae9b79ca3003dd05152911efb1407561df344")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
