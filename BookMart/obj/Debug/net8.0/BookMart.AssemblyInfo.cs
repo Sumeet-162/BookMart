dc6c4c97f720c500e841b14cef4e7c82e3dd5117
Sumeet-162 <sumeet2703@gmail.com>
2025-06-14 15:34:24 +0530

    Add Dockerfile for Render deployment

--- BookMart/obj/Debug/net8.0/BookMart.AssemblyInfo.cs
+++ BookMart/obj/Debug/net8.0/BookMart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+170083223332c44be5baa22caa36b7439aa6ca47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240bdea2d220685cc763160160911d56d7405be0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
